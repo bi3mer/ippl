a47a3f5fe0a460797f7501b68189e967cdd3f717
bi3mer <colan@comcast.net>
2020-01-22 15:43:33 -0500

    problem 2

---
+++ problem_set_1/README.txt
@@ -0,0 +1,2 @@
+;; Colan Biemer, Avijit Ghosh
+;; biemer.c@husky.neu.edu, avijit@ccs.neu.edu 
--- problem_set_1/problem_2.rkt
+++ problem_set_1/problem_2.rkt
@@ -6,7 +6,7 @@
   (n (node x))
   (e (edge x x))
   (x variable-not-otherwise-mentioned))
- 
+
 (define g1 (term (graph (node a) (node b) (node c)
                         (edge b a) (edge b c))))
 (define g2 (term (graph (node a) (node b)
@@ -15,14 +15,26 @@
 (test-equal (redex-match? Graph g g1) #t)
 (test-equal (redex-match? Graph g g2) #t)
 
+;; TODO: fix language
+;; TODO: add citation note
+(define-metafunction Lambda
+  in : x (x ...) -> boolean
+  [(in x (x_1 ... x x_2 ...)) #t]
+  [(in x (x_1 ... ) ) #f])
+
+;(test-equal (term (in x (x y z))) #t)
+;(test-equal (term (in x (y z))) #f)
+
 ;; Design the function good, which determines whether or not the edges in a Graph
-;; g mention only names that also name a node in g.
+;; g mention only names that also name a node in g. We changed it to good? since
+;; it returns a boolean.
 (define-metafunction Graph
   good? : g -> boolean
-  [(good? g) #t])
+  [(good? (graph n ... e ...)) #t])
 
+;(test-equal (term (good? (graph () ()))) #t)
 (test-equal (term (good? ,g1)) #t)
-(test-equal (term (good? ,g2)) #f)
+;(test-equal (term (good? ,g2)) #f)
 
 ;; run tests
 (test-results)

44316d9c0dfaf70495cd562fbf27588777e4e312
bi3mer <colan@comcast.net>
2020-01-22 13:31:33 -0500

    init

---
+++ problem_1.rkt
@@ -0,0 +1,4 @@
+(define-language mobile
+  (m w        ; atomic sculpture (weight)
+     (m m w)) ; composite
+  (w number)) ; weight is a number
---
+++ problem_set_1/problem_1.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+(require redex)
+
+(define-language mobile
+  (m w        ; atomic sculpture (weight)
+     (m m w)) ; composite
+  (w number)) ; weight is a number

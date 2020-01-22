fc95e01480e69f0f4b2d4ae50f3de6e5dd72bd88
bi3mer <colan@comcast.net>
2020-01-22 14:13:54 -0500

    num atomics

--- problem_set_1/problem_1.rkt
+++ problem_set_1/problem_1.rkt
@@ -6,15 +6,30 @@
      (m m w)) ; composite
   (w number)) ; weight is a number
 
-
-(redex-match? mobile m (term 3))
-
 (define m1 (term 3))
 (define m2 (term 4))
-(redex-match? mobile m (term (,m1 ,m2 5)))
+(define m3 (term (,m1 ,m2 5)))
+
+(test-equal (redex-match? mobile m (term 3)) #t)
+(test-equal (redex-match? mobile m (term (,m1 ,m2 5))) #t)
+(test-equal (redex-match? mobile m (term ,m3)) #t)
+
+;; num-atomics
+(define-metafunction mobile
+  num-atomics : m -> natural
+  [(num-atomics w) 1]
+  [(num-atomics (m_1 m_2 w)) ,(+ (+ (term (num-atomics m_1)) (term (num-atomics m_2))) 1)])
+
+(test-equal (term (num-atomics ,m1)) 1)
+(test-equal (term (num-atomics ,m2)) 1)
+(test-equal (term (num-atomics ,m3)) 3)
 
 ;; receives a mobile and returns a boolean to represent whether the weight of all
 ;; components on the left side are equal to the right side.
 
 
 ;; (test-equal mobile (term (balanced? )))
+
+
+
+(test-results)

ebf557eb830a846ef5a5e02a2b4ccefec20752e4
bi3mer <colan@comcast.net>
2020-01-22 13:52:47 -0500

    push

--- problem_set_1/problem_1.rkt
+++ problem_set_1/problem_1.rkt
@@ -1,8 +1,20 @@
 #lang racket
-
 (require redex)
 
 (define-language mobile
   (m w        ; atomic sculpture (weight)
      (m m w)) ; composite
   (w number)) ; weight is a number
+
+
+(redex-match? mobile m (term 3))
+
+(define m1 (term 3))
+(define m2 (term 4))
+(redex-match? mobile m (term (,m1 ,m2 5)))
+
+;; receives a mobile and returns a boolean to represent whether the weight of all
+;; components on the left side are equal to the right side.
+
+
+;; (test-equal mobile (term (balanced? )))

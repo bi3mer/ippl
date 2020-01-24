a8d451dfb9bd04f1279c4dc65f242255a21d9c2e
Avijit Ghosh <avijitg22@gmail.com>
2020-01-24 08:09:32 -0500

    Problem 2 done

--- problem_set_1/problem_2.rkt
+++ problem_set_1/problem_2.rkt
@@ -8,34 +8,16 @@
   (x variable-not-otherwise-mentioned))
 
 
-(define-language Node
-  (g (n ...))
-  (n (node x))
-  (x variable-not-otherwise-mentioned))
-
 (define g1 (term (graph (node a) (node b) (node c)
                         (edge b a) (edge b c))))
 (define g2 (term (graph (node a) (node b)
                         (edge b a) (edge b c))))
 
 
-;;(redex-match Node g g1)
 
 (test-equal (redex-match? Graph g g1) #t)
 (test-equal (redex-match? Graph g g2) #t)
 
-;; TODO: fix language
-;; based off of in function from tutorial I did: https://docs.racket-lang.org/redex/redex2015.html
-#;(define-metafunction Graph
-  in : x (e ...) -> boolean
-  [(in x (edge x_1 x_2) ... (edge x x_3 x_4) ...) #t]
-  [(in x (edge x_1 x_2) ... (edge x_3 x x_4) ...) #t]
-  [(in x (e_1 ... ) ) #f])
-
-#;(define-metafunction Graph
-  contains-node : x (e ...) -> boolean
-  [(contains-node x ()) #f]
-  [(contains-node x (e ...))])
 
 
 (define (slist->string slst)
@@ -43,26 +25,45 @@
 
 
 (define-metafunction Graph
-  nodes : g -> String
-  [(nodes (graph n ... e ...)) ,(slist->string (term(n ...)))])
+  nodes : g -> (n ...)
+  [(nodes (graph e ...)) ()]
+  [(nodes (graph n_1 n ... e ...)) ,(cons (term n_1) (term (nodes (graph n ... e ...))))])
 
-;;(regexp-replace* #rx"node *" "(node a) (node b) (node c)" "")
-;;  [(nodes (graph n ... e ...)) ,(regexp-replace* #rx"node *" slist->string(term(n ...)) "")])
+(define-metafunction Graph
+  nodesplit : (n ...) -> (x ...)
+  [(nodesplit ()) ()]
+  [(nodesplit ((node x_1) n ...)) ,(append (list(term x_1))(term (nodesplit (n ...))))])
 
 
-(term (nodes ,g1))
-;;(redex-match Node g (term (nodes ,g1)))
+(define-metafunction Graph
+  edges : g -> (e ...)
+  [(edges (graph n ...)) ()]
+  [(edges (graph n ... e_1 e ...)) ,(cons (term e_1) (term (edges (graph n ... e ...))))])
+
+(define-metafunction Graph
+  edgesplit : (e ...) -> (x ...)
+  [(edgesplit ()) ()]
+  [(edgesplit ((edge x_1 x_2) e ...)) ,(append (list(term x_1)(term x_2))(term (edgesplit (e ...))))])
+
+
+;;(term (nodes ,g1))
+;;(term (nodesplit(nodes ,g1)))
+;;(list->set(term (nodesplit(nodes ,g1))))
+;;(term (edges ,g1))
+;;(term (edgesplit(edges ,g1)))
+;;(list->set(term (edgesplit(edges ,g1))))
+
 
 ;; Design the function good, which determines whether or not the edges in a Graph
 ;; g mention only names that also name a node in g. We changed it to good? since
 ;; it returns a boolean.
 (define-metafunction Graph
   good? : g -> boolean
-  [(good? (graph n ... e ...)) #t])
+  [(good? (graph n ... e ...)) ,(set=? (list->set(term (nodesplit(nodes (graph n ... e ...))))) (list->set(term (edgesplit(edges (graph n ... e ...))))))])
 
-;(test-equal (term (good? (graph () ()))) #t)
+
 (test-equal (term (good? ,g1)) #t)
-;(test-equal (term (good? ,g2)) #f)
+(test-equal (term (good? ,g2)) #f)
 
 ;; run tests
 (test-results)

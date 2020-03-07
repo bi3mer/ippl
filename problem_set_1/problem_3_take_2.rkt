370f2d18e44d90f6804472a00450a8e8d7ba15a8
bi3mer <colan@comcast.net>
2020-03-07 18:42:07 -0500

    changed function and updated one line that had... 85 characters!!!!!

--- problem_set_1/problem_3_take_2.rkt
+++ problem_set_1/problem_3_take_2.rkt
@@ -57,57 +57,64 @@
 (test-equal (term (extend ,list "c" 0)) (term (("a" 0) ("b" 1) ("c" 0))))
 
 (define-metafunction DBExpression
-  db : d ((x n) ...) -> d
+  dbenv : d ((x n) ...) -> d
   ;; x is in lookup
-  [(db x ((x_1 n) ...))
+  [(dbenv x ((x_1 n) ...))
    (lookup ((x_1 n) ...) x)
    (side-condition (term (exists ((x_1 n) ...) x)))]
   
   ;; x is not in lookup
-  [(db x ((x_1 n) ...))
+  [(dbenv x ((x_1 n) ...))
    x
    (side-condition (eqv? #f (term (exists ((x_1 n) ...) x))))]
 
-  ;; add lookup and extend lookup
-  [(db (function x d) ((x_1 n) ...))
-   (function x (db d (addone (extend ((x_1 n) ...) x -1))))]
+  ;; add lookup and extend lookup (if x is already in that it will be overset to
+  ;; -1. The addone call sets the -1 to 0.
+  [(dbenv (function x d) ((x_1 n) ...))
+   (function x (dbenv d (addone (extend ((x_1 n) ...) x -1))))]
 
   ;; two expressions next to eachother
-  [(db (d_1 d_2) ((x n) ...)) ((db d_1 ((x n) ...)) (db d_2 ((x n) ...)))])
+  [(dbenv (d_1 d_2) ((x n) ...))
+   ((dbenv d_1 ((x n) ...))
+    (dbenv d_2 ((x n) ...)))])
 
-;; all tests check values and that the input langauge is an Expression and the
-;; output is a DBExpression.
+(define-metafunction DBExpression
+  db : d -> d
+  [(db d) (dbenv d ())])
+
+;; all tests check values and that the input langauge is an Expression and
+;; the output is a DBExpression.
 ;;
 ;; x remains even if there is a value in the store.
 (define in1 (term "x"))
 (test-equal (redex-match? Expression e in1) #t)
-(define test1 (term (db ,in1 (("y" 0)))))
+(define test1 (term (dbenv ,in1 (("y" 0)))))
 (test-equal test1 (term "x"))
 (test-equal (redex-match? DBExpression d test1) #t)
 
 ;; x changes when there is a valid value for it in the store.
-(define test2 (term (db ,in1 (("x" 0)))))
+(define test2 (term (dbenv ,in1 (("x" 0)))))
 (test-equal test2 (term 0))
 (test-equal (redex-match? DBExpression d test2) #t)
 
 ;; y remains in a function call with x.
 (define in3 (term (function "x" "y")))
 (test-equal (redex-match? Expression e in3) #t)
-(define test3 (term (db ,in3 ())))
+(define test3 (term (db ,in3)))
 (test-equal test3 (term (function "x" "y")))
 (test-equal (redex-match? DBExpression d test3) #t)
 
 ;; x changes to 0 when there is a binding occurrence.
 (define in4 (term (function "x" "x")))
 (test-equal (redex-match? Expression e in4) #t)
-(define test4 (term (db ,in4 ())))
+(define test4 (term (db ,in4)))
 (test-equal test4 (term (function "x" 0)))
 (test-equal (redex-match? DBExpression d test4) #t)
 
 ;; nothing should happen with to expressions next to eachother.
 (define in5 (term ("x" "y")))
 (test-equal (redex-match? Expression e in5) #t)
-(define test5 (term (db ,in5 ())))
+(define test5 (term (db ,in5)))
 (test-equal test5 (term ("x" "y")))
 (test-equal (redex-match? DBExpression d test5) #t)
 
@@ -115,14 +122,14 @@
 ;; that the store doesn't persist.
 (define in6 (term ((function "x" "x") (function "y" "x"))))
 (test-equal (redex-match? Expression e in6) #t)
-(define test6 (term (db ,in6 ())))
+(define test6 (term (db ,in6)))
 (test-equal test6 (term ((function "x" 0) (function "y" "x"))))
 (test-equal (redex-match? DBExpression d test6) #t)
 
 ;; sampel test from problem to show that it works as expected.
 (define in7 (term (function "y" ((function "x" ("x" "y")) ("x" "y")))))
 (test-equal (redex-match? Expression e in7) #t)
-(define test7 (term (db ,in7 ())))
+(define test7 (term (db ,in7)))
 (test-equal test7 (term (function "y" ((function "x" (0 1)) ("x" 0)))))
 (test-equal (redex-match? DBExpression d test7) #t)
 

261b53b3c6acb9c04e4a15f8a8eb08fd79dc1aff
bi3mer <colan@comcast.net>
2020-03-26 15:13:15 -0400

    variable assignment for reals and ints without handling bounds, yet.

--- stan.rkt
+++ stan.rkt
@@ -8,6 +8,10 @@
 ;; TODO: test vector bounding
 ;; TODO: test matrix bounding
 ;; TODO: test array bounding
+;; TODO: arithmetic operations
+;; TODO: int to r but not r to int
+;; TODO: for loop
+;; TODO: if test
 
 ;; NOTES:
 ;; - the current approach for restrictions does allow a user to create multiple
@@ -15,10 +19,11 @@
 ;;   nothing wrong with giving a variable 100 lower bounds. The implementation
 ;;   will only use the last one but this could be seen as a limitation to the
 ;;   implementation provided.
-(define-language STAN
-  (e ::= pv x (e ...) (e [e]))
-  (s ::= (T x = e)  skip) ;; (s ...) (for(e in e : e) s) (if e s then s)
 
+;;;; Stan language
+(define-language STAN
+  (e ::= pv x (e ...) (e [e])) ;; 
+  (s ::= skip (T x = e) (s ...)) ;;  (for(x in e : e) s) (if e s then s)
   
   ;; primative values
   (pv ::= integer number)
@@ -59,9 +64,16 @@
   ;; boolean operators
   (BO ::= > >= <= <= == !=)
 
+  ;; Operation Semantics
+  (E ::=
+     hole
+     (skip ... E s ...)
+     (T x = E))
+
   ;; variable
   (x ::= variable-not-otherwise-mentioned))
 
+;;;; stan environment
 (define-extended-language STAN_E STAN
   (σ ::= ((x V) ...)))
 
@@ -77,6 +89,28 @@
 (define-metafunction STAN
   lookup : ((x V) ...) x -> V
   [(lookup (_ ... (x V) _ ...) x) V])
+
+;;;; Required Metafunctions
+;; arithmetic
+;; boolean
+
+;;;; Reduction Relation
+(define stan_r
+  (reduction-relation
+   STAN_E
+   ;; Variables, still need type hcecking and bounds handling with the
+   ;; the T in there.
+   (--> [(in-hole E x) σ]
+       [(in-hole E (lookup σ x)) σ]
+       findVar)
+   (--> [(in-hole E (T x = V)) σ]
+        [(in-hole E skip) (extend σ x V)]
+        assign)
+
+   ;; 
+   ))
+
+
 
 ;;;;;;;;;;;;;;; Grammar Tests ;;;;;;;;;;;;;;;
 ;; test expressions
@@ -142,6 +176,29 @@
   STAN s (term ((r ((lower = 1) ((upper = 3) ((offset = 10) (multiplier = 2))))) x = 1)))
  #t)
 
+;; test list of statements
+(test-equal
+ (redex-match? STAN s (term (skip skip)))
+ #t)
+
+(test-equal
+ (redex-match? STAN s (term (skip ((i none) x = 3))))
+ #t)
+
+(test-equal
+ (redex-match? STAN s (term (((r none) x = 3) skip)))
+ #t)
+
+(test-equal
+ (redex-match? STAN s (term (((r none) x = 3) ((i none) x = 3))))
+ #t)
+
+"commented out test since we don't have arithmetic yet"
+#;(test-equal
+ (redex-match? STAN s (term (((r none) x = 3) ((i none) x = (3 + x)))))
+ #t)
+
+
 ;;;;;;;;;;;;;;; Environment Tests ;;;;;;;;;;;;;;;
 (test-equal (term (extend () x 3)) (term ((x 3))))
 (test-equal (term (extend () x 3.12)) (term ((x 3.12))))
@@ -156,6 +213,22 @@
 ;;;;;;;;;;;;;;; Judgement Tests ;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;; Reduction Relation Tests ;;;;;;;;;;;;;;;
+;;;; Helper Meta functions
+;; get the environment from the result of an apply reduction relation
+(define-metafunction STAN_E
+  getEnv : ((s σ)) -> σ
+  [(getEnv  ((s σ))) σ])
+
+;; variable assignment
+(define res1 (apply-reduction-relation* stan_r (term (((i none) x = 3) ()))))
+(test-equal (term (lookup (getEnv ,res1) x)) 3)
+
+(define res2 (apply-reduction-relation* stan_r (term (((i none) x = 3.12) ()))))
+(test-equal (term (lookup (getEnv ,res2) x)) 3.12)
+
+(define res3 (apply-reduction-relation* stan_r (term ((((i none) x = 42) ((r none) y = 3.212)) ()))))
+(test-equal (term (lookup (getEnv ,res3) x)) 42)
+(test-equal (term (lookup (getEnv ,res3) y)) 3.212)
 
 ;;;;;;;;;;;;;;; Full Program Tests ;;;;;;;;;;;;;;;
 
